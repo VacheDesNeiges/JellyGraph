1beae4bfc2729c4619689a3dd2550afae0e3c87d
Dindon <vachedesneiges@gmail.com>
2024-09-17 15:53:21 +0200

    added coverage-report generation option in build script

--- build.sh
+++ build.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+COVERAGE_REPORT=false
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --coverage-report)
+            COVERAGE_REPORT=true
+            shift
+            ;;
+        *)
+            echo "Unknow option : $1"
+            exit 1
+            ;;
+    esac
+done
+
 mkdir -p build
 cd build
 
@@ -8,3 +24,12 @@ cmake $cmake_options ..
 ninja
 ninja install
 mv test ..
+
+if [ "$COVERAGE_REPORT" = true ]; then
+    cd ..
+    ./test
+    llvm-profdata-18 merge -sparse default.profraw -o coverage.profdata
+    llvm-cov-18 show ./test -instr-profile=coverage.profdata -format=html -output-dir=coverage-report
+    rm default.profraw
+    rm coverage.profdata
+fi
